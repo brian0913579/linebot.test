27fe76d59f1eefa01b70f963de3d490635cc891e
brian <cool.brian1206cool@gmail.com>
2025-05-01 05:00:36 +0800

    finish testing

--- insertUsertoDatabase.py
+++ insertUsertoDatabase.py
@@ -18,8 +18,8 @@ def remove_user(user_id):
     connection.close()  # Close the connection
 
 # Example usage to insert users:
-#insert_user('Uea6813ef8ec77e7446090621ebcf472a', 'admin_Brian')
+insert_user('Uea6813ef8ec77e7446090621ebcf472a', 'admin_Brian')
 #insert_user('U1d640cea545510e631396b5306ade151', 'cyn.18')
 
 # Example usage to remove a user:
-remove_user('Uea6813ef8ec77e7446090621ebcf472a')
+#remove_user('Uea6813ef8ec77e7446090621ebcf472a')
