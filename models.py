a8203c12df2353e8c6558a5840b6af4a726ce2bb
brian0913579 <48744883+brian0913579@users.noreply.github.com>
2025-05-07 00:10:58 +0800

    Remove legacy Python files from root directory

--- models.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def get_allowed_users():
-    connection = sqlite3.connect('users.db')
-    cursor = connection.cursor()
-    cursor.execute("SELECT user_id, user_name FROM allowed_users")
-    users = cursor.fetchall()
-    connection.close()
-    return {user[0]: user[1] for user in users}
