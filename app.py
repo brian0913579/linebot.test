c777fd8f0e9c5ab71d4f48f8543425ded7a6971c
brian0913579 <48744883+brian0913579@users.noreply.github.com>
2025-05-13 15:53:10 +0000

    Refactor MQTT TLS configuration to use environment variable for CA file

--- app.py
+++ app.py
@@ -216,10 +216,10 @@ def mqtt_test():
     mqtt_port = int(os.environ.get("MQTT_PORT"))
     mqtt_username = os.environ.get("MQTT_USERNAME")
     mqtt_password = os.environ.get("MQTT_PASSWORD")
-    mqtt_cafile = os.environ.get("MQTT_CAFILE", "emqxsl-ca.crt")
+    cafile = os.environ.get("MQTT_CAFILE")
 
     # Set up TLS and credentials if necessary
-    client.tls_set(ca_certs=mqtt_cafile)
+    client.tls_set(ca_certs=cafile)
     # client.tls_insecure_set(True)  # Allow self-signed certificates
     client.username_pw_set(mqtt_username, mqtt_password)
 
