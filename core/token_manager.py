60fdbc718f4881727debc61a67a3c47ae406a7b6
brian0913579 <48744883+brian0913579@users.noreply.github.com>
2025-05-13 15:43:36 +0000

    Add import for VERIFY_TTL and update store_action_token function to use it for expiry calculation

--- core/token_manager.py
+++ core/token_manager.py
@@ -1,6 +1,8 @@
 import secrets as py_secrets
 import time
 
+from config.config_module import VERIFY_TTL
+
 # token_manager.py
 
 TOKENS = {}
@@ -15,6 +17,14 @@ def generate_token(user_id):
     TOKENS[token_close] = (user_id, "close", time.time() + 300)
 
     return token_open, token_close
+
+
+def store_action_token(token: str, user_id: str, action: str) -> None:
+    """
+    Store an action token in memory with an expiry timestamp.
+    """
+    expiry = time.time() + VERIFY_TTL
+    TOKENS[token] = (user_id, action, expiry)
 
 
 def clean_expired_tokens():
